20d50cebc45ca8cf4a2c08a807d7d414a7a4191a
Polaris <264532369+lauraevan@users.noreply.github.com>
2026-09-17 17:25:59 -0500

    Refresh Minecraft Home icon asset

--- js/home-row-custom.js
+++ js/home-row-custom.js
@@ -15,7 +15,7 @@ const COVER = {
   'Hollow Knight: Silksong': 'assets/game-art/silksong-cover.png',
   'Elden Ring': 'assets/game-art/elden-ring-cover.jpg',
   'Red Dead Redemption 2': 'assets/game-art/rdr2-cover.jpg',
-  'Minecraft': 'assets/game-art/minecraft-cover-user.jpg',
+  'Minecraft': 'assets/game-art/minecraft-cover-user.jpg?v=2',
   'Fortnite': 'assets/game-art/fortnite-cover.jpg'
 };
 
