1d2b428c095af5537507eb8b698790d447d6a4fb
Polaris <264532369+lauraevan@users.noreply.github.com>
2026-09-19 20:46:19 -0500

    Replace Home RDR2 tile with Red Dead Redemption

--- js/home-row-custom.js
+++ js/home-row-custom.js
@@ -15,7 +15,7 @@ const COVER = {
   'Grand Theft Auto V': 'assets/game-art/gta-v-cover.jpg',
   'Hollow Knight: Silksong': 'assets/game-art/silksong-cover.png',
   'Elden Ring': 'assets/game-art/elden-ring-cover.jpg',
-  'Red Dead Redemption 2': 'assets/game-art/rdr2-cover.jpg',
+  'Red Dead Redemption': 'assets/stratus-covers/bs0095.jpg',
   'Minecraft': 'assets/game-art/minecraft-cover-user.jpg',
   'Fortnite': 'assets/game-art/fortnite-cover.jpg',
   'Cyberpunk 2077': 'https://store-images.s-microsoft.com/image/apps.47379.63407868131364914.bcaa868c-407e-42c2-baeb-48a3c9f29b54.89bb995b-b066-4a53-9fe4-0260ce07e894?h=900&q=95&w=600'
@@ -26,7 +26,7 @@ const HERO = {
   'Grand Theft Auto V': 'assets/game-art/gta-v-hero.jpg',
   'Hollow Knight: Silksong': 'assets/game-art/silksong-hero.jpg',
   'Elden Ring': 'assets/game-art/elden-ring-hero.jpg',
-  'Red Dead Redemption 2': 'assets/game-art/rdr2-hero.jpg',
+  'Red Dead Redemption': 'assets/stratus-covers/bs0095.jpg',
   'Minecraft': 'assets/game-art/minecraft-hero-keyart.jpg',
   'Fortnite': 'assets/game-art/fortnite-hero.jpg',
   'Cyberpunk 2077': 'https://store-images.s-microsoft.com/image/apps.34838.63407868131364914.bcaa868c-407e-42c2-baeb-48a3c9f29b54.1463028d-79fa-46e5-9fc2-63203992a4dc?h=1080&q=95&w=1920'
@@ -35,7 +35,7 @@ const HERO = {
 const SWAPS = [
   { from:'Subnautica 2', to:'Grand Theft Auto V', badge:'X|S' },
   { from:'Microsoft Edge', to:'Elden Ring', badge:'X|S' },
-  { from:'Mortal Kombat 1', to:'Red Dead Redemption 2', badge:'X|S' },
+  { from:'Mortal Kombat 1', to:'Red Dead Redemption', badge:'X|S' },
   { from:'Roblox', to:'Minecraft', badge:'X|S' }
 ];
 
@@ -44,7 +44,7 @@ const CLOUD_ALIASES = {
   'Grand Theft Auto V': ['grand theft auto v','gta v','gta 5','grand theft auto 5'],
   'Hollow Knight: Silksong': ['hollow knight silksong','silksong'],
   'Elden Ring': ['elden ring'],
-  'Red Dead Redemption 2': ['red dead redemption 2','rdr2'],
+  'Red Dead Redemption': ['red dead redemption','red dead redemption 1','rdr1'],
   'Minecraft': ['minecraft'],
   'Fortnite': ['fortnite'],
   'Cyberpunk 2077': ['cyberpunk 2077','cyberpunk2077']
@@ -435,7 +435,7 @@ function makeLibraryTile(){
 
   const mosaic = document.createElement('span');
   mosaic.className = 'ref-library-mosaic';
-  ['Grand Theft Auto V','Elden Ring','Red Dead Redemption 2','Minecraft'].forEach(title => {
+  ['Grand Theft Auto V','Elden Ring','Red Dead Redemption','Minecraft'].forEach(title => {
     const img = document.createElement('img');
     img.src = COVER[title];
     img.alt = '';
